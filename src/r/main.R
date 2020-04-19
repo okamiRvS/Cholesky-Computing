4f9c756c0059a82f9f7356d73ed678034da82746
Silvia Traversa <s.traversa98@gmail.com>
2020-04-19 18:06:05 +0200

    Update R code

--- src/r/main.R
+++ src/r/main.R
@@ -3,16 +3,29 @@ library("Matrix")
 # https://www.r-bloggers.com/5-ways-to-measure-running-time-of-r-code/
 
 # set workspace path
-# setwd("~/git/cholesky-computing/src/r")
+# setwd("../../git/cholesky-computing/src/r")
 
-matrix = readMM("../../data/ex15.mtx")  
+matrix <- readMM("../../data/ex15.mtx")  
 
-# start = Sys.time()
+start <- Sys.time()
 
 # https://www.rdocumentation.org/packages/Matrix/versions/1.2-18/topics/chol
-c = Matrix::chol(matrix)
-# if matrix is not positive definite, an error is signalled.
 
-# end = Sys.time()
-# time = end - start
+cholesky <- tryCatch(
+  {
+    Matrix::chol(matrix)
+  },
+  error = function(e){
+    print(e) # if matrix is not positive definite or symmetric, an error is signalled.
+  },
+  warning = function(w) {
+    print(w) # not positive definite matrix warning
+  }
+)
+
+end <- Sys.time()
+time <- end - start
+time
+
+print(cholesky)
 
