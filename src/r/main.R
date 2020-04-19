2062ffec1cc4c9c1cc80637671061327ce5e4fcc
Silvia Traversa <s.traversa98@gmail.com>
2020-04-19 13:30:49 +0200

    R update

--- src/r/main.R
+++ src/r/main.R
@@ -1,13 +1,18 @@
 library("Matrix")
 
-
-#https://www.r-bloggers.com/5-ways-to-measure-running-time-of-r-code/
+# https://www.r-bloggers.com/5-ways-to-measure-running-time-of-r-code/
 
 # set workspace path
 # setwd("~/git/cholesky-computing/src/r")
 
 matrix = readMM("../../data/ex15.mtx")  
 
-#https://www.rdocumentation.org/packages/Matrix/versions/1.2-18/topics/chol
-c = chol(matrix)
-#print(c)
+# start = Sys.time()
+
+# https://www.rdocumentation.org/packages/Matrix/versions/1.2-18/topics/chol
+c = Matrix::chol(matrix)
+# if matrix is not positive definite, an error is signalled.
+
+# end = Sys.time()
+# time = end - start
+
