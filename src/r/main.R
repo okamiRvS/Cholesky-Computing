5d19c93b4f909f6b2458002f3282b4881722f098
Silvia Traversa <s.traversa98@gmail.com>
2020-04-21 12:50:46 +0200

    R - linear system solver

--- src/r/main.R
+++ src/r/main.R
@@ -3,13 +3,16 @@ library("Matrix")
 # set workspace path
 # setwd("../../git/cholesky-computing/src/r")
 
+# read matrix
+
 A <- readMM("../../data/ex15.mtx")
 
-# find b 
+# find b for x to be [1, 1, ..., 1]
+
 xe <- matrix(1, 1, ncol(A))
 b <- xe %*% A
 
-
+# Cholesky decomposition
 # https://www.rdocumentation.org/packages/Matrix/versions/1.2-18/topics/chol
 
 R <- tryCatch(
@@ -24,9 +27,15 @@ R <- tryCatch(
   }
 )
 
-RRt <- R %*% t(R)
-x <- as.vector(solve(RRt,t(b)))
+# solve the linear system
+# R is upper triangular, so i can apply backward substitution
+# https://stat.ethz.ch/R-manual/R-devel/library/base/html/backsolve.html
 
-# https://www.rdocumentation.org/packages/pracma/versions/1.9.9/topics/Norm
+y <- backsolve(R, b, nrow(b))
+
+x <- forwardsolve(t(R), y)
+
 # relative error
+# https://www.rdocumentation.org/packages/pracma/versions/1.9.9/topics/Norm
+
 err <-  norm(x - xe, type = "2") / norm(xe, type = "2")
