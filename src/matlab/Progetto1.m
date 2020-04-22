71687a0daced1aad35ebf048ea5c851f1e8f577c
i.bettini <i.bettini@campus.unimib.it>
2020-04-22 09:29:27 +0200

    Cholesky decomposition

--- src/matlab/Progetto1.m
+++ src/matlab/Progetto1.m
@@ -1,7 +1,21 @@
+% read the matrix
 [A, rows, col, entries] = mmread('C:\Users\King\cholesky-computing\data\ex15.mtx');
 
-R = chol(A);
+% find b for x to be [1, 1, ..., 1]
+xe = ones(1, length(A));
+b = xe .* A;
 
+% tic-toc command used to compute the calculation time of x
+tic
+
+% Cholewski decomposition
+R = chol(A);
+x = R\(R'\b);
+
+time = toc;
+
+% relative error
+error = norm(x - xe)/norm(xe);
 
 
 
