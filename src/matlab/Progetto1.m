a6ce2653dde7d2884b73dd8a85899c0193782121
i.bettini <i.bettini@campus.unimib.it>
2020-04-24 11:13:56 +0200

    Memory

--- src/matlab/Progetto1.m
+++ src/matlab/Progetto1.m
@@ -1,6 +1,10 @@
 % read the matrix
 [A, rows, col, entries] = mmread('C:\Users\King\cholesky-computing\data\ex15.mtx');
 
+% memory calculation after loading the matrix
+initial = memory;
+initial_memory = initial.MemUsedMATLAB;
+
 % find b for x to be [1, 1, ..., 1]
 xe = ones(1, length(A));
 b = xe .* A;
@@ -13,6 +17,13 @@ R = chol(A);
 x = R\(R'\b);
 
 time = toc;
+
+% memory calculation after Cholewski decomposition
+final = memory;
+final_memory = final.MemUsedMATLAB;
+
+% memory needeed to solve the system
+system_memory = final_memory - initial_memory;
 
 % relative error
 error = norm(x - xe)/norm(xe);
