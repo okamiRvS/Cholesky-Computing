aaa5ff48b024041fefda066865a90eb2c37dbbda
i.bettini <i.bettini@gmail.com>
2020-04-25 17:42:57 +0200

    update

--- src/matlab/Progetto1.m
+++ src/matlab/Progetto1.m
@@ -1,13 +1,13 @@
 % read the matrix
-[A, rows, col, entries] = mmread('C:\Users\King\cholesky-computing\data\ex15.mtx');
+[A, rows, col, entries] = mmread('/Users/king/git/cholesky-computing/data/ex15.mtx');
 
 % memory calculation after loading the matrix
-initial = memory;
-initial_memory = initial.MemUsedMATLAB;
+%initial = memory;
+%initial_memory = initial.MemUsedMATLAB;
 
 % find b for x to be [1, 1, ..., 1]
-xe = ones(1, length(A));
-b = xe .* A;
+xe = ones(1, col);
+b = A * xe';
 
 % tic-toc command used to compute the calculation time of x
 tic
@@ -19,15 +19,16 @@ x = R\(R'\b);
 time = toc;
 
 % memory calculation after Cholewski decomposition
-final = memory;
-final_memory = final.MemUsedMATLAB;
+%final = memory;
+%final_memory = final.MemUsedMATLAB;
 
 % memory needeed to solve the system
-system_memory = final_memory - initial_memory;
+%system_memory = final_memory - initial_memory;
 
 % relative error
-error = norm(x - xe)/norm(xe);
+error = norm(x - xe')/norm(xe');
 
-
+% Graphic
+%semilogx(x, y);
 
 
