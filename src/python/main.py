440334b76fdd7651370b6a47f061de7400c7700c
@okamiRvS <okamirvs@gmail.com>
2020-04-25 17:54:39 +0200

    fix python things

--- src/python/main.py
+++ src/python/main.py
@@ -1,13 +1,29 @@
+# coding=utf-8
 import pdb
 import sys
 import scipy.io
 import scipy.sparse as sparse
 from scipy.sparse import linalg as splinalg
+from sksparse.cholmod import cholesky
 import numpy as np
 from numpy import linalg as LA
 import time
-#from scikits.sparse.cholmod import cholesky
 
+# To install scikit-sparse
+# IT WORKS ON LINUX, NOT ON WIN7, MAYBE IN WIN10
+# https://scikit-sparse.readthedocs.io/en/latest/overview.html
+# IF DOESN'T WORK FOLLOW THESE:
+#
+# AND THESE AS FOLLOW IN THIS ORDER
+# pip3 install scikits.bootstrap # maybe this not useful
+# pip3 install Cython
+# pip3 install scikits.sparse
+
+# To install Memory Profiler
+# https://pypi.org/project/memory-profiler/
+# python3 -m memory_profiler main.py
+
+# Guide
 # https://en.wikipedia.org/wiki/Path_(computing)
 # https://gist.github.com/Puriney/98544b779bcb815926f7acf87f537e61
 # https://github.com/benfred/implicit/blob/master/benchmarks/benchmark_als.py
@@ -28,31 +44,32 @@ def sparse_cholesky(A):
 	Scipy does not currently provide a routine for cholesky decomposition
 	of a sparse matrix. This functionality is available in numpy and scipy
 	for dense matrices and in scikits.sparse for sparse matrices, depending on your matrix.
-
-	factor = cholesky(A)
 	'''
+	
+	return cholesky(A)
 
-	# https://gist.github.com/omitakahiro/c49e5168d04438c5b20c921b928f1f5d
-	n = A.shape[0]
-	LU = splinalg.splu(A,diag_pivot_thresh=0) # sparse LU decomposition
-
-	try : 
-		resu = sparse.diags(LU.U.diagonal()**0.5)
-		final = LU.L.dot(resu)
-		return final
-	except :
-		print("The matrix is not positive definite")
-		time.sleep(10)
-		exit(1)
-
-def main():
+def read_matrix():
 	# Read in mtx file by scipy
 	coo_mat = scipy.io.mmread('../../data/ex15.mtx')
 
 	# Scipy matrix in coo layout can be easily converted to other types: csr and csc.
 	# csr_mat = coo_mat.tocsr(copy=True)
-	A = coo_mat.tocsc()
-	print(A.todense(), end="\n\n")
+	return coo_mat.tocsc()
+	#print(A.todense())
+	#pdb.set_trace()
+
+@profile
+def main():
+	A = read_matrix()
+	print("Number of zeros", A.nnz)
+
+	# If you just want the number of bytes of the array elements
+	print("The number of bytes: ", A.data.nbytes + A.indptr.nbytes + A.indices.nbytes)
+	print("A.data.nbytes", A.data.nbytes)
+	print("A.indptr.nbytes", A.indptr.nbytes)
+	print("A.indices.nbytes", A.indices.nbytes)
+
+	#pdb.set_trace()
 
 	# get dimensions of matrix
 	[xSize, ySize] = A.get_shape()
@@ -63,24 +80,36 @@ def main():
 	# vettore termini noti dato da A*xe = b
 	b = A.dot(xe)
 
+	'''
 	# no sparse_cholesky execution
 	start = time.process_time()
 	L = no_sparse_cholesky(A.todense())
 	end = time.process_time()
-	print("Time execution no_sparse_cholesky: " + str(end - start), end="\n\n")
-	print(np.dot(L, L.T.conj()), end="\n\n") # Verify
-	#print(LA.norm(x - xe)/LA.norm(xe))
-	pdb.set_trace()
-
+	print("Time execution no_sparse_cholesky: " + str(end - start))
+	print(np.dot(L, L.T.conj())) # Verify
+	
+	'''
 	# sparse_cholesky execution
 	start = time.process_time()
 	Ls = sparse_cholesky(A)
 	end = time.process_time()
-	print("Time execution sparse_cholesky: " + str(end - start), end="\n\n")
-	print(np.dot(Ls, Ls.T.conj()), end="\n\n") # Verify
-	pdb.set_trace()
 
+	x = Ls(b) # solves the equation Ax=b
 
+	print("The solution is: ")
+	print(x)
+	print('\n')
+	print("Time execution sparse_cholesky: " + str(end - start))
+	print("The error relative is: ", np.linalg.norm(x-xe)/np.linalg.norm(xe))
+
+	# matrice cholesky size
+	# prima e dopo 
+	
+	'''
+	spB = Ls.L()
+	bo = np.allclose(A.todense(),spB.dot(spB.T).todense()) # Just for verification
+	print(bo)
+	'''	
 
 if __name__ == "__main__":
 	main()
