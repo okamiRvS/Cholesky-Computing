6df69da4848544084e3bab244c70b4a1b36a618d
Silvia Traversa <s.traversa98@gmail.com>
2020-04-25 15:09:33 +0200

    Fix R

--- src/r/main.R
+++ src/r/main.R
@@ -9,8 +9,8 @@ A <- readMM("../../data/ex15.mtx")
 
 # find b for x to be [1, 1, ..., 1]
 
-xe <- matrix(1, 1, ncol(A))
-b <- xe %*% A
+xe <- rep(1, 1, ncol(A))
+b <-  A %*% xe  
 
 # Cholesky decomposition
 # https://www.rdocumentation.org/packages/Matrix/versions/1.2-18/topics/chol
@@ -27,13 +27,13 @@ R <- tryCatch(
   }
 )
 
+
 # solve the linear system
 # R is upper triangular, so i can apply backward substitution
 # https://stat.ethz.ch/R-manual/R-devel/library/base/html/backsolve.html
 
-y <- backsolve(R, b, nrow(b))
-
-x <- forwardsolve(t(R), y)
+y <- forwardsolve(t(R), b)
+x <- backsolve(R, y)
 
 # relative error
 # https://www.rdocumentation.org/packages/pracma/versions/1.9.9/topics/Norm
