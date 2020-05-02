09eb9f40866bf4031307f21ebd7e41733cbf4320
Umberto <okamirvs@gmail.com>
2020-05-02 14:41:54 +0200

    results of matlab

--- src/matlab/main.m
+++ src/matlab/main.m
@@ -17,44 +17,49 @@ err = [1:s]';
 for i=3 : length(directory)
     name = directory(i).name;
     
-    % read the matrix
-    tic;
-    [A, row, col, entries] = mmread("../../data/" + name);
-    import_time = toc;
-    info_A = whos('A');
+    try
+        % read the matrix
+        tic;
+        [A, row, col, entries] = mmread("../../data/" + name);
+        import_time = toc * 1000;
+        info_A = whos('A');
 
-    % find b for x to be [1, 1, ..., 1]
-    xe = ones(1, col);
-    b = A * xe';
+        % find b for x to be [1, 1, ..., 1]
+        xe = ones(1, col);
+        b = A * xe';
 
-    % Cholewski decomposition
-    tic;
-    R = chol(A);
-    cholesky_time = toc;
-    info_R = whos('R');
+        % Cholewski decomposition
+        tic;
+        R = chol(A);
+        cholesky_time = toc * 1000;
+        info_R = whos('R');
 
-    % R = full(R);
+        % R = full(R);
 
-    tic;
-    x = R\(R'\b);
-    solution_time = toc;
+        tic;
+        x = R\(R'\b);
+        solution_time = toc * 1000;
 
-    % relative error
-    error = norm(x - xe')/norm(xe');
-    
-    program(i-2) = "Matlab"
-    names(i-2) = name
-    import(i-2) = import_time
-    rows(i-2) = row
-    cols(i-2) = col
-    nonZeros(i-2) = nnz(A)
-    size(i-2) = info_A.bytes
-    chol_info(i-2) = cholesky_time
-    chol_size(i-2) = info_R.bytes
-    sol_time(i-2) = solution_time
-    err(i-2) = error
+        % relative error
+        error = norm(x - xe')/norm(xe');
+
+        program(i-2) = "Matlab";
+        names(i-2) = name;
+        import(i-2) = import_time;
+        rows(i-2) = row;
+        cols(i-2) = col;
+        nonZeros(i-2) = nnz(A);
+        size(i-2) = info_A.bytes;
+        chol_info(i-2) = cholesky_time;
+        chol_size(i-2) = info_R.bytes;
+        sol_time(i-2) = solution_time;
+        err(i-2) = error;
+    catch exception
+        warning(name + ": " + exception.message);
+    end     
+        
 end
 
-data = table(program, names, rows, cols, nonZeros, size, chol_info, chol_size, sol_time, err);
+data = table(program, names, import, rows, cols, nonZeros, size, chol_info, chol_size, sol_time, err);
 writetable(data,'results.csv','WriteRowNames',true);
 
